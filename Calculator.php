03ad0f0ee924e3a8dadf7bade0c88bfcc77b7e1b
Christian Ojeda <christian.cnod@gmail.com>
2023-03-11 14:08:40 -0300

    Agrega comentario a las funciones de clase Calculator.php

--- Calculator.php
+++ Calculator.php
@@ -2,18 +2,38 @@
 
 class Calculator
 {
+    /**
+     * Retorna la suma de dos números enteros
+     * @param int $a
+     * @param int $b
+     */
     public function add( int $a, int $b) : int 
     {
         return $a + $b;
     }
+    /**
+     * Retorna la resta de dos números enteros
+     * @param int $a
+     * @param int $b
+     */
     public function subtract( int $a, int $b) : int 
     {
         return $a - $b;
     }
+    /**
+     * Retorna la multiplicación de dos números enteros
+     * @param int $a
+     * @param int $b
+     */
     public function multiply( int $a, int $b) : int 
     {
         return $a * $b;
     }
+    /**
+     * Retorna la división de dos números enteros
+     * @param int $a
+     * @param int $b
+     */
     public function divide( int $a, int $b) : float
     {
         if ($b === 0) {
